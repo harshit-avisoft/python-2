b7bf12c2a9883d62118011b059ae2c92c78b635c
Ayush-Bhat09 <ayush.bhat@avisoft.io>
2025-11-19 16:20:59 +0530

    Add day2.py file

--- day2.py
+++ day2.py
@@ -3,27 +3,30 @@ Grades=["C","B","A","E","F"]
 Valid_grades=["A","B","C","D","E","F"]
 
 
-user_data=0
+value="Y"
 
-while(user_data!=4):
+while(value=="Y"):
  print("\n\n\n")
  print("1. add a new student")
  print("2. update student's record")
  print("3. remove a student")
- print("4. if you want to exit press ")
+ print("Student Data Updation .....")
+ print("\n")
  user_data=int(input("enter your choice : "))
 
  if user_data==1:
     name1=input("enter your name : ")
-    print("The valid grades are A B C D E F")
+    # print("The valid grades are A B C D E F")
     grade1=input("enter your grade : ")
     print("\n")
     
     if grade1 in Valid_grades:
         Name.append(name1)
         Grades.append(grade1)
+        print("updated data .... \n")
         for n, g in zip(Name, Grades):
          print(f"{n} : {g}")
+        value=input("\npress N to exit and Y to continue...") 
     else:
          print("invalid grades not allowed")
 
@@ -31,13 +34,15 @@ while(user_data!=4):
 
  if user_data==2:
     name1=input("enter your name : ")
-    print("The valid grades are A B C D E F")
+    # print("The valid grades are A B C D E F")
     grade1=input("enter your new grade : ")
     if name1 in Name and grade1 in Valid_grades:
         index=Name.index(name1)
         Grades[index]=grade1
-        print(Name)
-        print(Grades)
+        print("updated data .... \n")
+        for n, g in zip(Name, Grades):
+         print(f"{n} : {g}")
+        value=input("\npress N to exit and  Y to continue...")    
     else:
         print("student doesn't exist or invalid grades")
 
@@ -49,7 +54,9 @@ while(user_data!=4):
         index=Name.index(name1)
         Name.pop(index)
         Grades.pop(index)
-        print(Name)
-        print(Grades)
+        print("\n updated data .... \n")
+        for n, g in zip(Name, Grades):
+         print(f"{n} : {g}")
+        value=input("\n press N to exit and  Y to continue...")
     else:
         print("student doesn't exist")
