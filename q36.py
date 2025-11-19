0a353524715f49fcaa049228902d4c213d0eeb86
Ayush-Bhat09 <ayush.bhat@avisoft.io>
2025-11-19 11:02:40 +0530

    Add q36.py

---
+++ q36.py
@@ -0,0 +1,12 @@
+num1=float(input("enter first decimal number : "))
+num2=float(input("enter second decimal number : "))
+
+if num1>num2:
+    print(f"{num1} is greater")
+else:
+    print(f"{num2} is greater")
+
+
+
+
+git remote add origin https://github.com/harshit-avisoft/python-2.git
