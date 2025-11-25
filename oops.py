53da7f8a07fd82ce2762ed928985f05e46afa622
Ayush-Bhat09 <ayush.bhat@avisoft.io>
2025-11-25 18:58:39 +0530

    oops

--- oops.py
+++ oops.py
@@ -1,38 +1,14 @@
-# 1
-# class car:
-#     def __init__(self,brand,model,year):
-#         self.brand=brand
-#         self.model=model
-#         self.year=year
+class Person:
+  def __init__(self, fname, lname):
+    pass
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
     
-#     def details(self):
-#         print(self.brand)
-#         print(self.model)
-#         print(self.year)
-    
-# c1=car("realme","narjo",2021)
-# c2=car("samsung","galaxy",2015)
-# c1.details()
 
-# 5
-# class Account:
-#    def withdraw(self):
-#       print("withdraw amount 0")
-
-# class SavingsAccount(Account):
-#     def withdraw(self):
-#       print("withdraw amount 1000")
-
-# class CurrentAccount(Account):
-#    def withdraw(self):
-#       print("withdraw amount 10000")
-
-# a1=Account()
-# s1=SavingsAccount()
-# c1=CurrentAccount()
-# a1.withdraw()
-# s1.withdraw()
-# c1.withdraw()
-
-
-# 6
+x = Student("Mike", "Olsen")
+x.printname()
